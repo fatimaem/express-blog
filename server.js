e8f6781ad9275bd3b162bd84bd5b914b55ae707a
fatima <fatimaem@code.edu.az>
2023-02-04 22:40:18 +0400

    added routes

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.get('/',(req,res) => {
+    res.render("index")
+})
+app.listen(9000)
